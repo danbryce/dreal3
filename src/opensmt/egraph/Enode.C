825ab2abe418d9be056eab0ece67238c770798f7
Daniel Bryce <danbryce410@gmail.com>
2014-07-31 13:42:41 -0500

    fix(ode_solver): fixed bug in capd formulation that involve double negative --

--- src/opensmt/egraph/Enode.C
+++ src/opensmt/egraph/Enode.C
@@ -330,7 +330,7 @@ void Enode::print_infix(ostream & os, lbool polarity, string const & variable_po
         }
     } else if (isTerm()) {
         // output "("
-        if (!getCdr()->isEnil() && (isPlus() || isMinus() || isTimes() || isPow())) {
+      if (!getCdr()->isEnil() && (isPlus() || isMinus() || isTimes() || isPow() || isDiv())) {
             os << "(";
         }
         // !(X = Y) ==> (0 = 0)
@@ -400,7 +400,7 @@ void Enode::print_infix(ostream & os, lbool polarity, string const & variable_po
             }
         }
         // output ")"
-        if (!getCdr()->isEnil() && (isPlus() || isMinus() || isTimes() || isPow())) {
+        if (!getCdr()->isEnil() && (isPlus() || isMinus() || isTimes() || isPow() || isDiv())) {
             os << ")";
         }
     } else if (isList()) {
